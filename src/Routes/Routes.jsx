dccf25462e432f23704d7e41e8e97cfcdd6e2dd2
Kenshin <517701884@qq.com>
2018-12-19 17:20:03 +0800

    fix 404

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Bundle from './Bundle';
 
 import Home from 'bundle-loader?lazy&name=Home!Pages/Home/Home.jsx';
+import Page404 from 'bundle-loader?lazy&name=Page404!Pages/Page404/Page404.jsx';
 
 
 const Loading = function () {

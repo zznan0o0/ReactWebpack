9883d47b65ccff07959525e331ba4150a7bea957
Kenshin <517701884@qq.com>
2019-01-30 14:39:10 +0800

    create

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
 
 import { Route, Switch } from 'react-router-dom';
-import Bundle from './Bundle';
+import Bundle, {createComponent} from './Bundle';
 
 import Home from 'bundle-loader?lazy&name=Home!Pages/Home/Home.jsx';
 import Page404 from 'bundle-loader?lazy&name=Page404!Pages/Page404/Page404.jsx';
-
-
-const Loading = function () {
-  return <div>Loading...</div>
-};
-
-const createComponent = (component) => (props) => (
-  <Bundle load={component}>
-    {
-      (Component) => Component ? <Component {...props} /> : <Loading />
-    }
-  </Bundle>
-);
 
 const Routes = (
   <Switch>
